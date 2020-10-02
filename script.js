572729b030a71235705f32ccfd61537368ea9f58
Sandy Ruby <thecodingfiles@gmail.com>
2020-10-02 17:56:08 +0100

    added condition if length value is null, set up intial validateSelection function

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ var userLower = document.querySelector("#lower");
 var userUpper = document.querySelector("#upper");
 var userNumber = document.querySelector("#number");
 var userSpecial = document.querySelector("#special");
+var generateBtn = document.querySelector("#generate");
 
 
 // Write validation function which outputs error message if min or max of userLength is violated
 function validateLength() {
   var errorMessage1 = document.querySelector("#lengthError");
-  if (this.checkValidity() === false) {
-    errorMessage1.innerHTML = "Value must be between 8-128";
+  if (this.checkValidity() === false || this.value == "") { // NEED TO WRITE CONDITION IF NULL
+    errorMessage1.innerHTML = "Password length cannot be empty and must be between 8-128";
+   // console.log("did not select correct value");
   } else {
     errorMessage1.innerHTML = "";
+  //  console.log("selected correct value");
   }
 }
 // Generate length validation function
@@ -20,7 +23,22 @@ userLength.addEventListener("blur", validateLength);
 
 
 
-var generateBtn = document.querySelector("#generate");
+// Write validation function to check if at least one input is selected
+function validateSelection() {
+    if (!document.getElementById("upper").checked) {
+      var errorMessage2 = document.querySelector("#selectionError");
+      // alert("pleast tick atleast one"); 
+      //console.log("did not select upper");
+    } else {
+     //console.log("did select upper");
+      errorMessage2 = "";
+    }
+  };
+// Generate length validation function
+generateBtn.addEventListener("click", validateSelection);
+
+/*
+
 // Write password to the #password input
 function writePassword() { //need to validate if user checked at least one checkbox
   var password = generatePassword(); // need to create a function to do this
@@ -32,6 +50,6 @@ function writePassword() { //need to validate if user checked at least one check
 // Add event listener to generate button and trigger function
 generateBtn.addEventListener("click", writePassword);
 
-
+*/
 
 
