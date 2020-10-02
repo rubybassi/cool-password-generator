9e0b8191efd9fafd71102dd432dd6ce4708fa43f
Sandy Ruby <thecodingfiles@gmail.com>
2020-10-03 00:23:28 +0100

    added return statements to funtions and created generate password function

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-// Assignment code for user inputs
-var userLength = document.querySelector("#length");
-var userLower = document.querySelector("#lower");
-var userUpper = document.querySelector("#upper");
-var userNumber = document.querySelector("#number");
-var userSpecial = document.querySelector("#special");
+// Declare button and user length variables
 var generateBtn = document.querySelector("#generate");
-
-
+var userLength = document.querySelector("#length");
 
 
 // Write validation function which outputs error message if min or max of userLength is violated
@@ -14,10 +8,12 @@ function validateLength() {
   var errorMessage1 = document.querySelector("#lengthError");
   if (userLength.checkValidity() === false || userLength.value === "") { 
     errorMessage1.innerHTML = "Password length cannot be empty and must be between 8-128";
-    // console.log("did not select correct value");
+    //console.log("did not select correct value");
+    return false;
   } else {
     errorMessage1.innerHTML = "";
     // console.log("selected correct value");
+    return true;
   }
 }
 
@@ -29,39 +25,66 @@ function validateSelection() {
   if (checked.length === 0) {
     errorMessage2.innerHTML = "You must select at least one character type";
    // console.log('no checkboxes checked');
+   return false;
   } else {
     errorMessage2.innerHTML = "";
   //  console.log(checked.length + 'checkboxes checked');
+   return true;
   }
 }
 
 
-// Create character sets
+// Declare variables for character sets
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var upper = lower.toUpperCase();
 var number = "0123456789";
 var special = '!@#$%^&*()_+{}:"<>?\|[];\',./`~';
 
-// Write generate password function - need to see what user selected using eventlisterns .checked 
-// and create a new string, then loop to the lengh of the userLength and add in 
+
+// Write generate password function 
 function generatePassword() {
+  var length = document.querySelector("#length").value;
+  var userLower = document.querySelector("#lower").checked;
+  var userUpper = document.querySelector("#upper").checked;
+  var userNumber = document.querySelector("#number").checked;
+  var userSpecial = document.querySelector("#special").checked;
 
-
+// Check to see if character set selected and append to new string 
+  var possibleCharacters = "";
+  if (userLower) {
+    possibleCharacters += lower; 
+  }
+  if (userUpper) {
+    possibleCharacters += upper; 
+    }
+  if (userNumber) {
+    possibleCharacters += number; 
+    }
+  if (userSpecial) {
+    possibleCharacters += special; 
+    }
+  //console.log(possibleCharacters); //check characters
+  //console.log(possibleCharacters.length); //check length
+  
+// loop the string to get random characters equal to the user inputted length
+  var randomSelection = "";
+  for (var i = 0; i < length; i++) {
+      randomSelection += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+      //console.log(randomSelection);
+    }
+  return randomSelection;
 }
 
 
 // Write password to the #password input
 function writePassword() { 
-  validateLength(); // calls function
-  validateSelection(); // calls function
-  
-  var password = generatePassword(); // need to create a function to do this
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  var lenghtIsValid = validateLength(); // calls and checks function's boolean value
+  var selectionIsValid = validateSelection(); // calls and checks function's boolean value
+  if (lenghtIsValid && selectionIsValid) {
+      var password = generatePassword(); 
+      var passwordText = document.querySelector("#password");
+       passwordText.value = password;
+  } 
 }
 // Add event listener to generate button and trigger function
 generateBtn.addEventListener("click", writePassword);
-
-
-
-// need to add preventDefault() somewhere to stop the browser taking over - read up
