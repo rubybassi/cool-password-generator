cf325883a7a2069f5fe8814668510ac09fc3944e
Sandy Ruby <thecodingfiles@gmail.com>
2020-10-02 16:43:11 +0100

    added function to validate user lenght input

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ var userNumber = document.querySelector("#number");
 var userSpecial = document.querySelector("#special");
 
 
+// Write validation function which outputs error message if min or max of userLength is violated
+function validateLength() {
+  var errorMessage1 = document.querySelector("#lengthError");
+  if (this.checkValidity() === false) {
+    errorMessage1.innerHTML = "Value must be between 8-128";
+  } else {
+    errorMessage1.innerHTML = "";
+  }
+}
+// Generate length validation function
+userLength.addEventListener("blur", validateLength);
+
+
+
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() { //need to validate if user checked at least one checkbox
